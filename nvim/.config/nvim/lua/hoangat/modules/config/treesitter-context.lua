938f5ebf55d1f773384e437ec039e70ed5c292a2
hoangat4848 <hoangat4848@gmail.com>
2022-05-06 14:59:13 +0700

    feat(nvim): tweak treesitter context

--- nvim/.config/nvim/lua/hoangat/modules/config/treesitter-context.lua
+++ nvim/.config/nvim/lua/hoangat/modules/config/treesitter-context.lua
@@ -3,7 +3,7 @@ local context = safe_require "treesitter-context"
 context.setup {
   enable = true, -- Enable this plugin (Can be enabled/disabled later via commands)
   throttle = true, -- Throttles plugin updates (may improve performance)
-  max_lines = 0, -- How many lines the window should span. Values <= 0 mean no limit.
+  max_lines = 2, -- How many lines the window should span. Values <= 0 mean no limit.
   patterns = { -- Match patterns for TS nodes. These get wrapped to match at word boundaries.
     -- For all filetypes
     -- Note that setting an entry here replaces all other patterns for this entry.
